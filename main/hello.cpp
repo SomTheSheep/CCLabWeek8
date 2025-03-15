360a9099c439bc6fad2619a7ac798ec6864cfdf4
Som <SohamSalunke007@outlook.com>
2025-03-15 18:38:20 +0530

    Some changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this PES2UG22CS565  \n";
+  cout << "I have successfully built and run this PES2UG22CS565 whoa I did it again  \n";
   return 0;
 }
